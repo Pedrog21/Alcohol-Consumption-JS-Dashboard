53c2210b21e744ac370a51ee123d43d42a10d496
Pedro Gonçalves <pedrogoncalves@MacBook-Air-de-Pedro.local>
2018-12-06 22:27:13 +0000

    atualizado

--- chernoff.js
+++ chernoff.js
@@ -12,10 +12,42 @@
             .attr("stroke-width", 1)
             .attr("fill", "none") */
 
+
+
+
 function face_factory(classname, data, x, y)
 {
     //data format
     //{ "x": 0 - 1 , "y": 0 -1, "z": 0-1, "w":0-1 }
+
+    var tooltipmouth = d3.select("body")
+        .append("div")
+        .style("position", "absolute")
+        .style("z-index", "10")
+        .style("visibility", "hidden")
+        .style("color","white")
+        //.text("Mouth: " + data.y);
+        .text(function(){if (data.y >= 0.5) {return "Mouth: High number of Faillures"} else {return "Mouth: Low number of Faillures"}});
+
+        var tooltipeye= d3.select("body")
+            .append("div")
+            .style("position", "absolute")
+            .style("z-index", "10")
+            .style("color","white")
+            .style("visibility", "hidden")
+            //.text("Eyes: " + data.z)
+            .text(function(){if (data.z >= 0.5) {return "Eyes: > 18 years old"} else {return "Eyes:  < 18 years old"}});
+
+
+            var tooltiphead= d3.select("body")
+                .append("div")
+                .style("position", "absolute")
+                .style("z-index", "10")
+                .style("color","white")
+                .style("visibility", "hidden")
+                //.text("Head: " + data.x);
+                .text(function(){if (data.x >= 0.5) {return "Head: High Gdp"} else {return "Head: Low Gdp"}});
+
 
     var r = 20 + data.w * 50;
 
@@ -27,11 +59,12 @@ function face_factory(classname, data, x, y)
             .attr("transform", "translate(" + x + "," + y + ")");
 
 
+
     console.log("make head");
     var head_color = d3.scale.linear()
         .domain([0, 1])
         .interpolate(d3.interpolateRgb)
-        .range(["#ff0000", "#0000ff"]); /*Pôr as cores a mudar nas caras*/
+        .range(["#ff0000", "#0000ff"]);
 
 
     var head = face.append("svg:circle")
@@ -40,7 +73,10 @@ function face_factory(classname, data, x, y)
             .attr("fill-opacity", .8)
             .attr("stroke", "#000")
             .attr("stroke-width", 4)
-            .attr("r", r);
+            .attr("r", r)
+            .on("mouseover", function(){return tooltiphead.style("visibility", "visible");})
+            .on("mousemove", function(){return tooltiphead.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
+            .on("mouseout", function(){return tooltiphead.style("visibility", "hidden");});
 
     console.log("make mouth");
     var mouth_x = [0, .5, 1];
@@ -72,7 +108,10 @@ function face_factory(classname, data, x, y)
             .attr("stroke-width", 4)
             .attr("fill", "none")
             .attr("transform", "translate(" + [0, r/3] + ")")
-            .attr("d", mouth_line(mouth_x));
+            .attr("d", mouth_line(mouth_x))
+            .on("mouseover", function(){return tooltipmouth.style("visibility", "visible");})
+            .on("mousemove", function(){return tooltipmouth.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
+            .on("mouseout", function(){return tooltipmouth.style("visibility", "hidden");});
 
     console.log("create eyes")
     var eyer = r/10 + data.z * (r/3);
@@ -83,7 +122,10 @@ function face_factory(classname, data, x, y)
         .attr("fill", "none")
         .attr("stroke-width", 4)
         .attr("transform", "translate(" + [-r/2.5, -r/3] + ")")
-        .attr("r", eyer);
+        .attr("r", eyer)
+        .on("mouseover", function(){return tooltipeye.style("visibility", "visible");})
+        .on("mousemove", function(){return tooltipeye.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
+        .on("mouseout", function(){return tooltipeye.style("visibility", "hidden");});
 
     //eyer = r/10 + data.w * (r/3);
     var right_eye = face.append("svg:circle")
@@ -92,7 +134,10 @@ function face_factory(classname, data, x, y)
         .attr("fill", "none")
         .attr("stroke-width", 4)
         .attr("transform", "translate(" + [r/2.5, -r/3] + ")")
-        .attr("r", eyer);
+        .attr("r", eyer)
+        .on("mouseover", function(){return tooltipeye.style("visibility", "visible");})
+        .on("mousemove", function(){return tooltipeye.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
+        .on("mouseout", function(){return tooltipeye.style("visibility", "hidden");});
 
 
 
@@ -120,9 +165,9 @@ function clickypieNorte()
                              .append("svg:rect")
                                  .attr("width", "100%")
                                  .attr("height", "100%")
-                                 .attr("stroke", "white")
+                                 .attr("stroke", "dimgrey")
                                  .attr("stroke-width", 1)
-                                 .attr("fill", "white")
+                                 .attr("fill", "dimgrey")
               var data = { "x":0, "y":0.33, "z":0, "w":0.67 }
               face_factory("face"+count, data, 100, 100)
               toggleSelected = false;
@@ -160,9 +205,9 @@ function clickypieCentro()
                              .append("svg:rect")
                                  .attr("width", "100%")
                                  .attr("height", "100%")
-                                 .attr("stroke", "white")
+                                 .attr("stroke", "dimgrey")
                                  .attr("stroke-width", 1)
-                                 .attr("fill", "white")
+                                 .attr("fill", "dimgrey")
               var data = { "x":0.13, "y":0, "z":0.33, "w":0.67 }
               face_factory("face"+count, data, 100, 100)
               toggleSelected = false;
@@ -199,9 +244,9 @@ function clickypieAlgarve()
                              .append("svg:rect")
                                  .attr("width", "100%")
                                  .attr("height", "100%")
-                                 .attr("stroke", "white")
+                                 .attr("stroke", "dimgrey")
                                  .attr("stroke-width", 1)
-                                 .attr("fill", "white")
+                                 .attr("fill", "dimgrey")
               var data = { "x":0.5, "y":0.33, "z":1, "w":0.33 }
               face_factory("face"+count, data, 100, 100)
               toggleSelected = false;
@@ -237,9 +282,9 @@ function clickypieAlentejo()
                              .append("svg:rect")
                                  .attr("width", "100%")
                                  .attr("height", "100%")
-                                 .attr("stroke", "white")
+                                 .attr("stroke", "dimgrey")
                                  .attr("stroke-width", 1)
-                                 .attr("fill", "white")
+                                 .attr("fill", "dimgrey")
               var data = { "x":0.25, "y":0, "z":0.67, "w":1 }
               face_factory("face"+count, data, 100, 100)
               toggleSelected = false;
@@ -274,9 +319,9 @@ function clickypieLisboa()
                              .append("svg:rect")
                                  .attr("width", "100%")
                                  .attr("height", "100%")
-                                 .attr("stroke", "white")
+                                 .attr("stroke", "dimgrey")
                                  .attr("stroke-width", 1)
-                                 .attr("fill", "white")
+                                 .attr("fill", "dimgrey")
               var data = { "x":1, "y":0, "z":0.67, "w":0.33 }
               face_factory("face"+count, data, 100, 100)
               toggleSelected = false;
